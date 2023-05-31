2eae9123c545f2a3ba2b38bf1fd5fe8873af60a0
Antongz <sallyushaz@gmail.com>
2023-05-31 11:06:58 +0800

    feat: 简单适配vue版本的antd

--- src/index.ts
+++ src/index.ts
@@ -1,45 +1,36 @@
 import Less from 'less'
-import path from 'path'
-import { normalizePath } from './utils'
+import path from 'path';
+import { normalizePath } from './utils';
 
 // ref: less-plugin-module-resolver
 // https://github.com/bundle-matters/less-plugin-module-resolver/blob/main/src/alias-file-manager.ts
 
 export class LessPluginRemoveAntdGlobalStyles implements Less.Plugin {
-  constructor() {}
-
-  public install(_less: LessStatic, pluginManager: Less.PluginManager): void {
-    pluginManager.addFileManager(new FileManager())
+  install(less: LessStatic, pluginManager: Less.PluginManager): void {
+    pluginManager.addFileManager(new FileManager());
   }
 }
 
 class FileManager extends Less.FileManager {
-  constructor() {
-    super()
-  }
-  public supports(
+  supports(
     filename: string,
     currentDirectory: string,
-    _options: Less.LoadFileOptions,
-    _environment: Less.Environment
+    options: Less.LoadFileOptions,
+    environment: Less.Environment
   ): boolean {
     if (filename.includes('global')) {
-      // match antd global style file
-      const fullPath = normalizePath(path.join(currentDirectory, filename))
-      if (
-        fullPath.includes('antd/es/style/core/global') ||
-        fullPath.includes('antd/lib/style/core/global')
-      ) {
-        return true
-      }
+      const fullPath = normalizePath(path.join(currentDirectory, filename));
+      const isAntdGlobalStyle =
+        fullPath.includes('antd/') || fullPath.includes('ant-design-vue/');
+      if (isAntdGlobalStyle) return true;
     }
-    return false
+    return false;
   }
 
-  public async loadFile(): Promise<Less.FileLoadResult> {
+  async loadFile(): Promise<Less.FileLoadResult> {
     return {
       filename: 'dummy-empty-global-style.less',
       contents: '',
-    }
+    };
   }
 }
